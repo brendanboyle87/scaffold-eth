7a1519490d1df02a20c1cdf487ca4de65a7f2701
Brendan Boyle <brendanboyle87@gmail.com>
2021-09-16 13:09:37 -0400

    initial stakng contract version, part 1

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,21 +2,23 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
+import "./ExampleExternalContract.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
 
-  //event SetPurpose(address sender, string purpose);
+ mapping(address => uint256) public balances;
 
-  string public purpose = "Building Unstoppable Apps!!!";
+ uint256 public constant threshold = 1 ether;
+
+ event Stake(address indexed sender, uint256 amount);
 
   constructor() {
-    // what should we do on deploy?
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      //emit SetPurpose(msg.sender, purpose);
-  }
+ function stake() public payable {
+   balances[msg.sender] += msg.value;
+
+   emit Stake(msg.sender, msg.value);
+ }
 }
